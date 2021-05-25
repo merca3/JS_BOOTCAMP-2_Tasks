83b9fcdf0046c83701ba651b89cf87f02cc360d1
Katrina M <merca3@inbox.lv>
2021-05-25 16:40:58 +0300

    await_asyn_task

---
+++ 3.AsyncAwait_Task3_24.05.21/index.js
@@ -0,0 +1,98 @@
+import readline from 'readline';
+import { accessSync, writeFileSync } from 'fs';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const askFirstName = async() => {
+    return new Promise((fulfill, reject) => {
+        rl.question('Enter your first name: ', (firstName) => {
+            if (firstName === '') {
+                reject('Please fill the first name');
+                return;
+            }
+            fulfill(firstName);
+        })
+    });
+}
+const askLastName = async() => {
+    return new Promise((fulfill, reject) => {
+        rl.question('Enter your last name: ', (lastName) => {
+            if (lastName === '') {
+                reject('Please fill the last name');
+                return;
+            }
+            fulfill(lastName);
+        })
+    });
+}
+const askEmail = async() => {
+    return new Promise((fulfill, reject) => {
+        rl.question('Enter your email: ', (email) => {
+            if (email === '') {
+                reject('Please fill the email');
+                return;
+            }
+            fulfill(email);
+        })
+    });
+}
+const askAge = async() => {
+    return new Promise((fulfill, reject) => {
+        rl.question('Enter your age: ', (age) => {
+            if (age === '') {
+                reject('Please fill the age');
+                return;
+            }
+            if (isNaN(age)) {
+                reject('Age should be a number');
+                return;
+            }
+            if (parseInt(age) < 1) {
+                reject('Age should be valid');
+                return;
+            }
+            fulfill(parseInt(age));
+        })
+    });
+}
+const askAdress = async() => {
+    return new Promise((fulfill, reject) => {
+        rl.question('Enter your adress: ', (adress) => {
+            if (adress === '') {
+                reject('Please fill the adress');
+                return;
+            }
+            fulfill(adress);
+        })
+    });
+}
+
+const filePath = `${process.cwd()}/data.json`;
+
+try {
+    const firstName = await askFirstName();
+    const lastName = await askLastName();
+    const email = await askEmail();
+    const age = await askAge();
+    const adress = await askAdress();
+    console.log(`The data is: ${firstName}, ${lastName}, ${email}, ${age}, ${adress}`);
+
+    accessSync(filePath);
+
+    const newData = {
+        firstNameJSON: firstName,
+        lastNameJSON: lastName,
+        emailJSON: email,
+        ageJSON: age,
+        adressJSON: adress,
+    }
+    writeFileSync(filePath, JSON.stringify(newData));
+
+} catch (e) {
+    console.log(`Whoops, something went wrong. The error is: ${e}`);
+}
+
+rl.close();
