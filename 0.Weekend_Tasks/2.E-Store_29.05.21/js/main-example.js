f57824d7ab9ef22a28cb494efac1702d34dc9a3b
Katrina M <merca3@inbox.lv>
2021-06-07 21:27:57 +0300

    updated

--- 0.Weekend_Tasks/2.E-Store_29.05.21/js/main-example.js
+++ 0.Weekend_Tasks/2.E-Store_29.05.21/js/main-example.js
@@ -1,11 +1,13 @@
+// All happens only within #cart-item1 div
+
 $('.remove-btn').click(() => {
     $(this)
 })
 
-$('.add-amount').click(() => {
-    $(this)
-})
+// $('.add-amount').click(() => {
+//     $(this)
+// })
 
-$('.minus-amount').click(() => {
-    $(this)
-})
+// $('.minus-amount').click(() => {
+//     $(this)
+// })
