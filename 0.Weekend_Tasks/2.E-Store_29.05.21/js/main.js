9b6acbfb764fb5983e7ac16a7ce18f623ea15564
Katrina M <merca3@inbox.lv>
2021-06-07 18:42:57 +0300

    e-store_almost_final

--- 0.Weekend_Tasks/2.E-Store_29.05.21/js/main.js
+++ 0.Weekend_Tasks/2.E-Store_29.05.21/js/main.js
@@ -6,16 +6,19 @@ $('#remove-btn1').click(() => {
     const newTotalPay = parseFloat($('#total-pay').text()) - parseFloat($('#product-price1').text()) * parseInt($('#product-amount1').text());
     $('#total-pay').text(newTotalPay);
     $('#cart-item1').remove();
+    ifEmpty();
 })
 $('#remove-btn2').click(() => {
     const newTotalPay = parseFloat($('#total-pay').text()) - parseFloat($('#product-price2').text()) * parseInt($('#product-amount2').text());
     $('#total-pay').text(newTotalPay);
     $('#cart-item2').remove();
+    ifEmpty();
 })
 $('#remove-btn3').click(() => {
     const newTotalPay = parseFloat($('#total-pay').text()) - parseFloat($('#product-price3').text()) * parseInt($('#product-amount3').text());
     $('#total-pay').text(newTotalPay);
     $('#cart-item3').remove();
+    ifEmpty();
 })
 
 
@@ -27,6 +30,7 @@ $('#add-amount1').click(() => {
     $('#product-amount1').text(newAmount);
     $('#product-mult-price1').text(newTotalPrice);
     $('#total-pay').text(newTotalPay);
+    ifEmpty();
 })
 $('#minus-amount1').click(() => {
     const newAmount = parseInt($('#product-amount1').text()) - 1;
@@ -38,6 +42,7 @@ $('#minus-amount1').click(() => {
     if (newAmount < 1) {
         $('#cart-item1').remove();
     };
+    ifEmpty();
 })
 
 $('#add-amount2').click(() => {
@@ -47,7 +52,7 @@ $('#add-amount2').click(() => {
     $('#product-amount2').text(newAmount);
     $('#product-mult-price2').text(newTotalPrice);
     $('#total-pay').text(newTotalPay);
-
+    ifEmpty();
 })
 $('#minus-amount2').click(() => {
     const newAmount = parseInt($('#product-amount2').text()) - 1;
@@ -59,6 +64,7 @@ $('#minus-amount2').click(() => {
     if (newAmount < 1) {
         $('#cart-item2').remove();
     }
+    ifEmpty();
 })
 
 $('#add-amount3').click(() => {
@@ -68,6 +74,7 @@ $('#add-amount3').click(() => {
     $('#product-amount3').text(newAmount);
     $('#product-mult-price3').text(newTotalPrice);
     $('#total-pay').text(newTotalPay);
+    ifEmpty();
 })
 $('#minus-amount3').click(() => {
     const newAmount = parseInt($('#product-amount3').text()) - 1;
@@ -79,10 +86,24 @@ $('#minus-amount3').click(() => {
     if (newAmount < 1) {
         $('#cart-item3').remove();
     }
+    ifEmpty();
 })
 
-// if ($('#total-pay').text() == '0') {
-//     $('#empty-card').toggleClass('hidden');
-//     $('#total').toggleClass('hidden');
-//     $('#cart-list').toggleClass('hidden');
-// }
+
+
+const ifEmpty = () => {
+    if ($('#total-pay').text() == '0') {
+        $('#empty-card').removeClass('hidden');
+        $('.total').addClass('hidden');
+        $('.cart-list').addClass('hidden');
+    }
+}
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 56.946285, lng: 24.105078 },
+        zoom: 8,
+    });
+}
